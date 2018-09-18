c1269379cc2e5b0558d362e306451a96572606f9
KarthikeyanPommaiyan <43335435+KarthikeyanPommaiyan@users.noreply.github.com>
2018-09-18 14:25:19 +0530

    Create possitive or negative.c

---
+++ possitive or negative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter the number to check :");
+scanf("%d",&a);
+if(a>0)
+{
+printf("The number is possitive");
+}
+else
+{
+printf("The number is negative");
+}
+}
