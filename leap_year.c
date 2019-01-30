d851a25410f7ce4d199d9098529e622ad4a600f1
KarthikeyanPommaiyan <43335435+KarthikeyanPommaiyan@users.noreply.github.com>
2019-01-30 14:53:42 +0530

    Create leap_year.c

---
+++ leap_year.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int y;
+    scanf("%d", &y);
+    if( (y%4)==0 )
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}
