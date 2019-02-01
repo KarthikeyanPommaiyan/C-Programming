e5e4b8e4db0b3fc06f6982a521f47bb523457146
KarthikeyanPommaiyan <43335435+KarthikeyanPommaiyan@users.noreply.github.com>
2019-02-01 21:23:50 +0530

    Create no_of_hello.c

---
+++ no_of_hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i;
+    scanf("%d",&n);
+    if(n>0)
+    {
+    for(i=1;i<=n;i++)
+    {
+        printf("\nHello");
+    }
+    }
+}
